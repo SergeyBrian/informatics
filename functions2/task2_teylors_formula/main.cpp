55035334dba26a5448bac4778641dd0e0978b1d1
Sergio Brianski <s.brianski@yahoo.com>
2021-01-16 09:03:12 +0300

    Working on teylors formula

--- functions2/task2_teylors_formula/main.cpp
+++ functions2/task2_teylors_formula/main.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <cmath>
 
+#define _USE_MATH_DEFINES
+
 using namespace std;
 
-int factorial (int n)
-	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 
-double getSin(double a, int n) {
-	if (pow(-1,n)*(pow(a,2*n+1))/factorial(2*n+1)==a)
-		return 1;
-	return getSin(a, n-1);
+double deg2rad(double deg) {
+	return (deg*M_PI/180);
+}
+
+int factorial (int n) {
+	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+}
+
+double teylor(double a, int n) {
+	if (n == 1) return a;
+	double ns = (pow(-1,n)*(pow(a,2*n+1))/factorial(2*n+1));
+	cout << ns << endl;
+	return ns + teylor(a,n-1);
 }
 
 int main() {
 	int accuracy;
 	double angle;
-	cout << "Enter angle in radians: ";
+	cout << "Enter angle in degrees: ";
 	cin >> angle;
+	angle = deg2rad(angle);
 	cout << "Enter accuracy: ";
 	cin >> accuracy;
-	cout << "sin(" << angle << ") = " << getSin(angle, accuracy);
+	cout << "sin(" << angle << ") = " << teylor(angle, accuracy);
 	cout << endl;
+	cout << "Real value: " << sin(angle) << endl;
 	return 0;
 }
