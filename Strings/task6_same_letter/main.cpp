1e49b471705b730e6552037b2d03bf8bcef494dd
Sergio Brianski <s.brianski@yahoo.com>
2021-02-13 13:08:51 +0300

    Working on task 6 for strings

--- Strings/task6_same_letter/main.cpp
+++ Strings/task6_same_letter/main.cpp
@@ -8,12 +8,30 @@ int main () {
 	cout << "Enter string: ";
 	getline(cin,str);
 	str = "&" + str;
-	int letters[100];
+	char letters[100];
 	for (int i = 0; i < str.length(); i ++) {
 		if (str[i] == '&' || str[i] == ' ') {
-			
+			for (int j = 0; j < 100; j ++) {
+				if (letters[j] == str[i+1]) break;
+				if (!letters[j]) {
+					letters[j] = str[i+1];
+					break;
+				}
+			}
 		}
 	}
-
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < str.length(); j++) {
+			if ((str[j]=='&' || str[j]==' ') && 
+					str[j+1]==letters[i]) {
+				for (int k = j+1; k < str.length(); k++) {
+					cout << str[k];
+				}
+				cout << endl;
+			}
+		}
+		
+		if (!letters[i]) break;
+	}
 	return 0;
 }
